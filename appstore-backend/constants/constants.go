3fde956c96a8f2879d13666f4b147b0a3bd05ffe
WenqingZhong <zhongwenqing99@gmail.com>
2023-09-21 11:40:26 -0400

    appstore project

---
+++ appstore-backend/constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    ES_URL = "http://10.182.0.4:9200"
+    ES_USERNAME = "zhongwenqing99"
+    ES_PASSWORD = "zwq250527"
+    STRIPE_API_KEY = "sk_test_51NipQ6L9VOXxIlJdn8gtOuqbLAYEQp7F3HMxDseEZrPIEp6A9CZI1495clN4kZ45DIaoo41zuCkq2xIHyci4QWaV006zWXog2U"
+    GCS_BUCKET = "zhongwenqing99-appstore-bucket1"
+)
